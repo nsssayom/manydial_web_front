4833a01b94087efb8773cc2a16524cb3277714e3
nsssayom <nsssayom@gmail.com>
2021-07-05 20:23:47 +0600

    Pricing wrapping color chnaged

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -11,49 +11,51 @@
 		</div>
 
 		<div class="row">
-			<div class="col-md-3">
-				<div class="pricing-container text-center">
-					<div class="pricing-header">
-						<h3>সিলভার</h3>
-						<p>১-১০০ নাম্বারে কল ব্রডকাস্ট করার জন্য</p>
-					</div>
-					<div class="pricing-body">
-						<ul>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								নিজস্ব অডিও
-							</li>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								স্বাভাবিক অ্যাপ্রুভাল টাইম
-							</li>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								কল রিপোর্ট
-							</li>
-						</ul>
-					</div>
-					<div class="pricing-footer">
-						<h3>৳৩.০০/মিনিট</h3>
-						<!-- <small>Monthly Payment</small> -->
+			<div class="col-xl-3 col-md-6 price-list">
+				<div class="pricing-wrapper wrapper-silver">
+					<div class="pricing-container text-center">
+						<div class="pricing-header">
+							<h3>সিলভার</h3>
+							<p>১-১০০ নাম্বারে কল ব্রডকাস্ট করার জন্য</p>
+						</div>
+						<div class="pricing-body">
+							<ul>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									নিজস্ব অডিও
+								</li>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									স্বাভাবিক অ্যাপ্রুভাল টাইম
+								</li>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									কল রিপোর্ট
+								</li>
+							</ul>
+						</div>
+						<div class="pricing-footer">
+							<h3>৳৩.০০/মিনিট</h3>
+							<!-- <small>Monthly Payment</small> -->
+						</div>
 					</div>
 				</div>
 			</div>
 
-			<div class="col-md-3">
-				<div class="pricing-wrapper">
+			<div class="col-xl-3 col-md-6 price-list">
+				<div class="pricing-wrapper wrapper-gold">
 					<div class="pricing-container">
 						<div class="pricing-header text-center">
 							<h3>গোল্ড</h3>
@@ -95,94 +97,100 @@
 				</div>
 			</div>
 
-			<div class="col-md-3">
-				<div class="pricing-container">
-					<div class="pricing-header text-center">
-						<h3>ডায়মন্ড</h3>
-						<p>১০০০ থেকে ১০০০০ নাম্বারে ব্রডকাস্ট করার জন্য</p>
-					</div>
-					<div class="pricing-body">
-						<ul>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								নিজস্ব অডিও
-							</li>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								দ্রুততম অ্যাপ্রুভাল টাইম
-							</li>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								কল রিপোর্ট
-							</li>
-						</ul>
-					</div>
-					<div class="pricing-footer">
-						<h3>৳১.০০/মিনিট</h3>
-						<!-- <small>Monthly Payment</small> -->
+			<div class="col-xl-3 col-md-6 price-list">
+				<div class="pricing-wrapper wrapper-platinum">
+					<div class="pricing-container">
+						<div class="pricing-header text-center">
+							<h3>প্ল্যাটিনাম</h3>
+							<p>১০০০ থেকে ১০০০০ নাম্বারে ব্রডকাস্ট করার জন্য</p>
+						</div>
+						<div class="pricing-body">
+							<ul>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									নিজস্ব অডিও
+								</li>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									দ্রুততম অ্যাপ্রুভাল টাইম
+								</li>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									কল রিপোর্ট
+								</li>
+							</ul>
+						</div>
+						<div class="pricing-footer">
+							<h3>৳১.০০/মিনিট</h3>
+							<!-- <small>Monthly Payment</small> -->
+						</div>
 					</div>
 				</div>
 			</div>
 
-			<div class="col-md-3">
-				<div class="pricing-container">
-					<div class="pricing-header text-center">
-						<h3>প্লাটিনাম</h3>
-						<p>১০০০০ এর বেশি নাম্বারে ব্রডকাস্ট করার জন্য</p>
-					</div>
-					<div class="pricing-body">
-						<ul>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								নিজস্ব অডিও
-							</li>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								দ্রুততম অ্যাপ্রুভাল টাইম
-							</li>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								কাস্টোমাইজ আইভিয়ার
-							</li>
-							<li>
-								<span>
-									<font-awesome-icon
-										:icon="['fas', 'check']"
-										:style="{ color: '#407BFF' }"
-								/></span>
-								কল রিপোর্ট
-							</li>
-						</ul>
-					</div>
-					<div class="pricing-footer">
-						<a href="#contact-us" class="btn btn-primary full-width"
-							>কলরেট জানতে যোগাযোগ করুন</a
-						>
-						<!-- <small>Monthly Payment</small> -->
+			<div class="col-xl-3 col-md-6 price-list">
+				<div class="pricing-wrapper wrapper-diamond">
+					<div class="pricing-container">
+						<div class="pricing-header text-center">
+							<h3>ডায়ামন্ড</h3>
+							<p>১০০০০ এর বেশি নাম্বারে ব্রডকাস্ট করার জন্য</p>
+						</div>
+						<div class="pricing-body">
+							<ul>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									নিজস্ব অডিও
+								</li>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									দ্রুততম অ্যাপ্রুভাল টাইম
+								</li>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									কাস্টোমাইজ আইভিয়ার
+								</li>
+								<li>
+									<span>
+										<font-awesome-icon
+											:icon="['fas', 'check']"
+											:style="{ color: '#407BFF' }"
+									/></span>
+									কল রিপোর্ট
+								</li>
+							</ul>
+						</div>
+						<div class="pricing-footer">
+							<a
+								href="#contact-us"
+								class="btn btn-primary full-width"
+								>কলরেট জানতে যোগাযোগ করুন</a
+							>
+							<!-- <small>Monthly Payment</small> -->
+						</div>
 					</div>
 				</div>
 			</div>
@@ -196,5 +204,35 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.pricing-container {
+	min-height: 522px;
+}
+.pricing-body {
+	min-height: 207px;
+}
+.price-list {
+	padding-bottom: 30px;
+}
+
+.wrapper-silver {
+	background: linear-gradient(#4285f4 30%, #ea4335 64%) no-repeat;
+}
+
+.wrapper-gold {
+	background: linear-gradient(#4285f4 30%, #fbbc04 64%) no-repeat;
+}
+
+.wrapper-platinum {
+	background: linear-gradient(#4285f4 30%, #34a853 64%) no-repeat;
+}
+
+.wrapper-diamond {
+	background: linear-gradient(#4285f4 30%, #7b1fa2 64%) no-repeat;
+}
+
+.pricing-wrapper {
+	padding: 4px;
+	border-radius: 4px;
+}
 </style>
